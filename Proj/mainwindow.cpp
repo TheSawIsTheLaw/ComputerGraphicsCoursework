effef4bac65c40e96223a4e2f0871aaabc11d4af
TheSawIsTheLaw <dimakrok@bk.ru>
2020-07-11 17:45:25 +0300

    ADD: items for list widg

--- Proj/mainwindow.cpp
+++ Proj/mainwindow.cpp
@@ -6,6 +6,19 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->listWidget->addItem("Стол");
+    ui->listWidget->addItem("Высокий стол");
+    ui->listWidget->addItem("Стул");
+    ui->listWidget->addItem("Высокий стул");
+    ui->listWidget->addItem("Диван");
+    ui->listWidget->addItem("Растение в горшке");
+    ui->listWidget->addItem("Подиум");
+    ui->listWidget->addItem("Экран для проектора");
+    ui->listWidget->addItem("Плазменный телевизор");
+    ui->listWidget->addItem("Шкаф");
+    ui->listWidget->addItem("Стеллаж");
+    ui->listWidget->addItem("Источник света");
 }
 
 MainWindow::~MainWindow()
