63d5d81377b7c24cc9129acb907755e0353947d7
TheSawIsTheLaw <dimakrok@bk.ru>
2020-08-15 13:13:12 +0300

    ADD: 33 commit'a 33 commit'a v kajdom izmenenii lish' odna stroka

--- Proj/usagefacade.cpp
+++ Proj/usagefacade.cpp
@@ -4,6 +4,8 @@
 
 void UsageFacade::setCellScene(size_t width_, size_t length_)
 {
+    if (scene)
+        delete scene;
     scene = new CellScene(width_, length_);
     qDebug("Set was done\n");
 }
