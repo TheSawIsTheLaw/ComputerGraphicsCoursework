39b8f39f5f1255bb37525d7bddd8b0b9b5748a2c
TheSawIsTheLaw <dimakrok@bk.ru>
2020-08-15 15:08:49 +0300

    ADD: initialization for fields of CellScene

--- Proj/objects.cpp
+++ Proj/objects.cpp
@@ -10,5 +10,10 @@ CellScene::CellScene(size_t width_, size_t length_)
     for (size_t i = 0; i < length_; i++)
     {
         cells.push_back(std::vector<bool>(width_));
+        for (size_t j = 0; j < width_; j++)
+            cells[i][j] = true;
     }
+
+    modelsNum = 0;
+    illumNum = 0;
 }
