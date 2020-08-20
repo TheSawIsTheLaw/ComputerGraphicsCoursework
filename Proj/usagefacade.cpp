444b4e7fd6fc6c0b51cdf8d28b8588870f314ae2
TheSawIsTheLaw <dimakrok@bk.ru>
2020-08-20 18:08:09 +0300

    ADD: hmm. Some... Alg?....

--- Proj/usagefacade.cpp
+++ Proj/usagefacade.cpp
@@ -3,6 +3,8 @@
 #include "QDebug"
 #include "QPen"
 
+#include "math.h"
+
 #include "config.hpp"
 
 UsageFacade::UsageFacade()
@@ -43,16 +45,19 @@ void Drawer::zBufferAlg(CellScene *scene, size_t bufLength, size_t bufWidth)
 
     for (size_t i = 0; i < bufLength; i++)
     {
-        depthBuffer.push_back(std::vector<size_t>(bufWidth));
-        frameBuffer.push_back(std::vector<size_t>(bufWidth));
+        depthBuffer.push_back(
+        std::vector<size_t>(bufWidth, std::numeric_limits<size_t>::max()));
+        frameBuffer.push_back(std::vector<size_t>(bufWidth, 0));
     }
+
+    qDebug() << "Frame Vec: " << frameBuffer;
 
     PolModel plate = scene->getPlateModel();
     std::vector<Facet> facets = plate.getFacets();
     std::vector<Vertex> vertices = plate.getVertices();
     std::array<Dot3D, 3> dotsArr;
 
-    for (std::vector<Facet>::iterator iter = facets.begin(); iter != facets.end() ; iter++)
+    for (std::vector<Facet>::iterator iter = facets.begin(); iter != facets.end(); iter++)
     {
         dotsArr[0] = vertices.at(iter->getUsedDots().at(0)).getPosition();
         dotsArr[1] = vertices.at(iter->getUsedDots().at(1)).getPosition();
@@ -74,8 +79,39 @@ void Drawer::zBufferAlg(CellScene *scene, size_t bufLength, size_t bufWidth)
         }
 
         qDebug() << "SORTED DOTS ARE:" << dotsArr[0] << dotsArr[1] << dotsArr[2];
-    }
 
+        for (double curY = dotsArr[0].getYCoordinate();
+             curY < dotsArr[2].getYCoordinate(); curY++)
+        {
+            double x1 = dotsArr[0].getXCoordinate();
+            double x2 = dotsArr[1].getXCoordinate();
+            double x3 = dotsArr[2].getXCoordinate();
+
+            double z1 = dotsArr[0].getZCoordinate();
+            double z2 = dotsArr[1].getZCoordinate();
+            double z3 = dotsArr[2].getZCoordinate();
+
+            double aInc = (curY - dotsArr[0].getYCoordinate()) /
+                          (dotsArr[1].getYCoordinate() - dotsArr[0].getYCoordinate());
+            double bInc = (curY - dotsArr[0].getYCoordinate()) /
+                          (dotsArr[2].getYCoordinate() - dotsArr[0].getYCoordinate());
+
+            double xA = x1 + (x2 - x1) * aInc;
+            double xB = x1 + (x3 - x1) * bInc;
+            double zA = z1 + (z2 - z1) * aInc;
+            double zB = z1 + (z3 - z1) * bInc;
+
+            for (double curX = xA; curX < xB; curX++)
+            {
+                double curZ = zA + (zB - zA) * (curX - xA) / (xB - xA);
+                if (curZ < depthBuffer[round(curX)][round(curY)])
+                {
+                    depthBuffer[round(curX)][round(curY)] = curZ;
+                    frameBuffer[round(curX)][round(curY)] = 0;
+                }
+            }
+        }
+    }
 }
 
 QGraphicsScene *Drawer::drawScene(CellScene *scene, QRectF rect)
