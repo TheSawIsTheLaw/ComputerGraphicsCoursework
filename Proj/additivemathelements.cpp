49c2af9e15912627b32041f4c28da72a9f9e9848
TheSawIsTheLaw <dimakrok@bk.ru>
2020-08-31 16:26:49 +0300

    ADD: scale transformation method realization LOL

--- Proj/additivemathelements.cpp
+++ Proj/additivemathelements.cpp
@@ -20,6 +20,32 @@ void Dot3D::moveDot(double xDif, double yDif, double zDif)
     zCoordinate += zDif;
 }
 
+void Dot3D::scaleDot(double coef, double xCenter, double yCenter, double zCenter)
+{
+    xCoordinate -= xCenter;
+    yCoordinate -= yCenter;
+    zCoordinate -= zCenter;
+
+    Eigen::Matrix4f scaleMatrix;
+    // clang-format off
+    scaleMatrix << coef, 0, 0, 0,
+                   0, coef, 0, 0,
+                   0, 0, coef, 0,
+                   0, 0,    0, 1;
+    // clang-format on
+    Eigen::MatrixXf coordinatesVec(1, 4);
+    coordinatesVec << xCoordinate, yCoordinate, zCoordinate, 1;
+
+//    std::cout << scaleMatrix << "\n\n" << coordinatesVec;
+
+    coordinatesVec = coordinatesVec * scaleMatrix;
+    xCoordinate = coordinatesVec(0, 0) + xCenter;
+    yCoordinate = coordinatesVec(0, 1) + yCenter;
+    zCoordinate = coordinatesVec(0, 2) + zCenter;
+
+//    std::cout << "\n\n" << coordinatesVec * scaleMatrix << "\n-----------\n";
+}
+
 std::ostream &operator<<(std::ostream &os, const Dot3D &dot)
 {
     os << "Dot3D: (" << dot.xCoordinate << "; " << dot.yCoordinate << "; "
